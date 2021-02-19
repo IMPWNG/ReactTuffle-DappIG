c2253c39ee657ffcdc92c758bf46332fdb40c1b8
IMPWONG <matisse.acheen.pro@gmail.com>
2021-02-19 14:52:42 -0500

    Commit 1

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,35 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+ 
+pragma solidity ^0.7.4;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = "Decentragram"; 
+
+  //Store Images
+
+  uint public imageCount = 0;
+  mapping(uint => Image) public images;
+
+  struct Image {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+  //Create Images
+
+  
+  
+  function uploadImage(string memory _imgHash, string memory _description) public {
+
+    //Increment image id
+    imageCount = imageCount ++;
+
+    //Add image ton contract
+    images[1] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+  }
+
+  //Tip Images 
 }
